1ec48fcc751dd8c08e078bd0c0ae58b6468ea7da
vchlinnn <chuclinh190305@gmail.com>
2025-02-12 20:53:15 -0600

    fix blocks flying up due to inverted coordinate yay

--- src/main/java/com/gamewerks/blocky/gfx/BlockyPanel.java
+++ src/main/java/com/gamewerks/blocky/gfx/BlockyPanel.java
@@ -39,7 +39,8 @@ public class BlockyPanel extends JPanel {
                 for (int col = 0; col < 4; col++) {
                     if (layout[row][col]) {
                         g.fillRect((activePos.col + col) * BLOCK_SIZE,
-                                   (activePos.row - row + 1) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                                    (Constants.BOARD_HEIGHT - 2 - (activePos.row - row + 1)) * BLOCK_SIZE,  
+                                    BLOCK_SIZE, BLOCK_SIZE);
                     }
                 }
             }
@@ -49,7 +50,7 @@ public class BlockyPanel extends JPanel {
         for (int row = 0; row < Constants.BOARD_HEIGHT; row++) {
             for (int col = 0; col < Constants.BOARD_WIDTH; col++) {
                 if (well[row][col]) {
-                    g.fillRect(col * BLOCK_SIZE, BLOCK_SIZE * (row + 1), BLOCK_SIZE, BLOCK_SIZE);
+                    g.fillRect(col * BLOCK_SIZE, (Constants.BOARD_HEIGHT - 3 - row) * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
                 }
             }
         }
